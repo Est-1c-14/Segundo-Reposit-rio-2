464fb1bce53939601845e0dfcdde3b61e5344770
João Victor <joao.victor.rodrigues01@escola.pr.gov.br>
2025-05-28 22:17:07 -0300

    28/05/25

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao{
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+}
+
+criaCartao{
+    'Geografia',
+    'Qual é a capital da França?',
+    'O Python é uma linguagem de programação'
+}
+
+criaCartao{
+    'Programação',
+    'O que é um função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+}
+ 
+criaCartao{
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+}
+
+criaCartao{
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização'
+}
