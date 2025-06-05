b682afe62c9fecdbe1e89a33a3485b314b260737
João Victor <joao.victor.rodrigues01@escola.pr.gov.br>
2025-06-05 19:51:20 -0300

    05/06/25

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ function criaCartao(categoria, pergunta, resposta) {
     }
     cartao.addEventListener('click', viraCartao);
 
-    container.appendChild(cartao)
+    container.appendChild(cartao);
 }
