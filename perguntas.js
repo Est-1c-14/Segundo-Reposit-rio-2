5a8f3233e473cdce73d030fb3decf7d11f17628f
João Victor <joao.victor.rodrigues01@escola.pr.gov.br>
2025-06-05 19:59:26 -0300

    05/06/25 ;D

--- perguntas.js
+++ perguntas.js
@@ -22,8 +22,8 @@ criaCartao{
     'O JavaScript é uma linguagem de programação'
 }
 
-criaCartao{
-    'Programação',
-    'O que é CSS?',
-    'O CSS é uma linguagem de estilização'
-}
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em inglês é HI (rai)'
+)
