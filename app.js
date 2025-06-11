cd24121f45bd141bd956aa0b3d56e87554a3921b
João Victor <joao.victor.rodrigues01@escola.pr.gov.br>
2025-06-11 20:10:33 -0300

    11/06/25

--- app.js
+++ app.js
@@ -17,11 +17,12 @@ function criaCartao(categoria, pergunta, resposta) {
 
     let respostaEstaVisivel = false;
 
-    function viraCartao() {
+    function viraCartao () {
         respostaEstaVisivel = !respostaEstaVisivel;
         cartao.classList.toggle('active', respostaEstaVisivel);
     }
-    cartao.addEventListener('click', viraCartao);
+    cartao.addEventListener('click', viraCartao)
 
+    
     container.appendChild(cartao);
 }
--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,30 @@
-criaCartao{
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-}
-
-criaCartao{
-    'Geografia',
-    'Qual é a capital da França?',
-    'Paris'
-}
-
-criaCartao{
-    'Programação',
-    'O que é um função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-}
- 
-criaCartao{
-    'Programação',
-    'O que é JavaScript?',
-    'O JavaScript é uma linguagem de programação'
-}
-
 criaCartao(
+    'Programação',
+    'Oque é Python?',
+    'é uma linguagem de Programação'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao (
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao (
+    'Peogramação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguage de Programação'
+)
+
+criaCartao (
     'Lingua inglesa',
-    'Como se diz OI em inglês?',
+    'Como se diz OI em inglês',
     'Oi em inglês é HI (rai)'
+
 )
