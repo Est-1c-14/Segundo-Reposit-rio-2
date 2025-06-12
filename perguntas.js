732d5dc40198a11e1cff58e3b7bde8b9760f531d
João Victor <joao.victor.rodrigues01@escola.pr.gov.br>
2025-06-11 21:13:20 -0300

    teste criacartao

--- perguntas.js
+++ perguntas.js
@@ -28,3 +28,92 @@ criaCartao (
     'Oi em inglês é HI (rai)'
 
 )
+criaCartao(
+  'HTML',
+  'O que significa HTML?',
+  'HTML significa HyperText Markup Language, usado para estruturar páginas web.'
+)
+
+criaCartao(
+  'HTML',
+  'Como criar um link em HTML?',
+  'Usa-se a tag <a> com o atributo href, por exemplo: <a href="url">texto</a>.'
+)
+
+criaCartao(
+  'HTML',
+  'Qual a função da tag <div>?',
+  'A tag <div> serve para agrupar elementos HTML e aplicar estilos ou scripts.'
+)
+
+criaCartao(
+  'HTML',
+  'O que é um atributo em HTML?',
+  'Um atributo fornece informações adicionais para um elemento, como id, class ou src.'
+)
+
+criaCartao(
+  'HTML',
+  'Como inserir uma imagem em uma página HTML?',
+  'Usa-se a tag <img> com o atributo src, ex: <img src="imagem.jpg" alt="descrição">.'
+)
+
+criaCartao(
+  'JavaScript',
+  'O que é JavaScript?',
+  'JavaScript é uma linguagem de programação usada para criar interatividade em páginas web.'
+)
+
+criaCartao(
+  'JavaScript',
+  'Como declarar uma variável em JavaScript?',
+  'Usa-se var, let ou const, por exemplo: let nome = "Maria";'
+)
+
+criaCartao(
+  'JavaScript',
+  'Qual a diferença entre == e === em JavaScript?',
+  '== compara valores com conversão de tipo, === compara valor e tipo sem conversão.'
+)
+
+criaCartao(
+  'JavaScript',
+  'O que é uma função em JavaScript?',
+  'Função é um bloco de código que pode ser executado quando chamado.'
+)
+
+criaCartao(
+  'JavaScript',
+  'Como adicionar um evento de clique em JavaScript?',
+  'Usa-se addEventListener ou o atributo onclick, ex: element.addEventListener("click", func);'
+)
+
+criaCartao(
+  'CSS',
+  'O que significa CSS?',
+  'CSS significa Cascading Style Sheets, usado para estilizar páginas HTML.'
+)
+
+criaCartao(
+  'CSS',
+  'Como selecionar um elemento pelo id em CSS?',
+  'Usa-se o símbolo # seguido do id, ex: #meuId { color: red; }'
+)
+
+criaCartao(
+  'CSS',
+  'Qual a diferença entre class e id em CSS?',
+  'id é único para um elemento, class pode ser usada em múltiplos elementos.'
+)
+
+criaCartao(
+  'CSS',
+  'Como mudar a cor do texto em CSS?',
+  'Usa-se a propriedade color, ex: color: blue;'
+)
+
+criaCartao(
+  'CSS',
+  'O que é o box model em CSS?',
+  'Box model define margens, bordas, padding e conteúdo de um elemento.'
+)
